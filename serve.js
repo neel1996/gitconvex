7548c42bebf8ea7fce208c9f202593bbf2d3cb59
Neel1996 <neelusenthil1996@gmail.com>
2020-06-23 05:49:24 +0000

    serve route change

--- serve.js
+++ serve.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, 'build', "index.html"));
 });
 
