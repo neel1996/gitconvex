763cf3cd1ff6f43cfa5c6b1771210a7f6c28da08
Neel1996 <neelusenthil1996@gmail.com>
2020-06-26 04:36:56 +0000

    Homepage fix in package.json and re-render state fix for GitOperationComponent,,- Added empty path to homepage in package json file,- Added View reload state as a dependency to useEffect to make the component render for every file un-staging and staging ,- Multi line commit message fix for commit component

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a1061a9d44445ea0b8cd03b9176f9874.js"
+  "/precache-manifest.40c33a53cdf48feb19ee33e56919bbd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
