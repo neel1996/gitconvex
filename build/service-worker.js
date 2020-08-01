141e77379a35b5cd3dc1927f5f15462d5ee0df54
Neel1996 <neelusenthil1996@gmail.com>
2020-08-01 13:17:59 +0000

    Added transition to the home button in repo details. Updated README with setup guidelines

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c0667506f34ce1eadfd047a99a97748.js"
+  "/precache-manifest.04d702b709dcb5bbb24e5bd0524ebd90.js"
 );
 
 self.addEventListener('message', (event) => {
