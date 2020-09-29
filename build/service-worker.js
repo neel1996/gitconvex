0202d1b1e48ceb0d4e3aaf02198695cfc83bec1e
Neel1996 <neelusenthil1996@gmail.com>
2020-09-29 18:57:32 +0000

    fix for commit compare bug (#90)

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7af63e721719ca5056af63b0702fde8.js"
+  "/precache-manifest.077bf448256794231c19e6e2b066140a.js"
 );
 
 self.addEventListener('message', (event) => {
