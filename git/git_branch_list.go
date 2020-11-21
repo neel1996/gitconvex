0d5a027db03107cebd9ea5b78867f55efc1a9122
Neel1996 <neelusenthil1996@gmail.com>
2020-11-21 18:12:07 +0000

    add logic to handle nil references and branches and log errors

--- git/git_branch_list.go
+++ git/git_branch_list.go
@@ -42,46 +42,53 @@ func GetBranchList(repo *git.Repository, branchChan chan Branch) {
 			splitCurrentBranch := strings.Split(currentBranch, "/")
 			currentBranch = splitCurrentBranch[len(splitCurrentBranch)-1]
 
-			bIter, _ := repo.Branches()
-
 			ref, _ := repo.References()
-			_ = ref.ForEach(func(reference *plumbing.Reference) error {
-				var (
-					refNamePtr *string
-				)
 
-				if ref != nil {
-					if reference.Name().String() != "HEAD" && strings.Contains(reference.Name().String(), "refs/") {
-						refNameSplit := strings.Split(reference.Name().String(), "refs/")
-						if len(refNameSplit) == 2 {
-							logger.Log(fmt.Sprintf("Available Branch : %v", refNameSplit[1]), global.StatusInfo)
-							if strings.Contains(refNameSplit[1], "heads/") {
-								headBranch := strings.Split(refNameSplit[1], "heads/")[1]
-								refNamePtr = &headBranch
-							} else {
-								refNamePtr = &refNameSplit[1]
+			if ref != nil {
+				_ = ref.ForEach(func(reference *plumbing.Reference) error {
+					var (
+						refNamePtr *string
+					)
+					if ref != nil {
+						if reference.Name().String() != "HEAD" && strings.Contains(reference.Name().String(), "refs/") {
+							refNameSplit := strings.Split(reference.Name().String(), "refs/")
+							if len(refNameSplit) == 2 {
+								logger.Log(fmt.Sprintf("Available Branch : %v", refNameSplit[1]), global.StatusInfo)
+								if strings.Contains(refNameSplit[1], "heads/") {
+									headBranch := strings.Split(refNameSplit[1], "heads/")[1]
+									refNamePtr = &headBranch
+								} else {
+									refNamePtr = &refNameSplit[1]
+								}
+								allBranchList = append(allBranchList, refNamePtr)
 							}
-							allBranchList = append(allBranchList, refNamePtr)
 						}
 					}
-				}
-
-				return nil
-			})
-
-			_ = bIter.ForEach(func(reference *plumbing.Reference) error {
-				if reference != nil {
-					localBranch := reference.String()
-					splitBranch := strings.Split(localBranch, "/")
-					localBranch = splitBranch[len(splitBranch)-1]
-
-					branches = append(branches, &localBranch)
 					return nil
-				} else {
-					return types.Error{Msg: "Empty reference"}
-				}
-			})
-			bIter.Close()
+				})
+			} else {
+				logger.Log("No references found!", global.StatusError)
+			}
+
+			bIter, _ := repo.Branches()
+
+			if bIter != nil {
+				_ = bIter.ForEach(func(reference *plumbing.Reference) error {
+					if reference != nil {
+						localBranch := reference.String()
+						splitBranch := strings.Split(localBranch, "/")
+						localBranch = splitBranch[len(splitBranch)-1]
+
+						branches = append(branches, &localBranch)
+						return nil
+					} else {
+						return types.Error{Msg: "Empty reference"}
+					}
+				})
+				bIter.Close()
+			} else {
+				logger.Log("Nil branch reference found", global.StatusError)
+			}
 		}
 
 		branchChan <- Branch{
