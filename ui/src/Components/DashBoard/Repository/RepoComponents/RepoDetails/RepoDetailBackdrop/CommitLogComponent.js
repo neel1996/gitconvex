f477b05e8a7c946452a644b6dd430e3928b8ae06
Neel1996 <neelusenthil1996@gmail.com>
2020-12-05 05:35:28 +0000

    fix tailwind build failure in actions yml

--- ui/src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CommitLogComponent.js
+++ ui/src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CommitLogComponent.js
@@ -52,7 +52,6 @@ export default function RepositoryCommitLogComponent(props) {
                       hash
                       author
                       commitMessage
-                      commitRelativeTime
                       commitFilesCount
                   }  
               }
@@ -114,7 +113,6 @@ export default function RepositoryCommitLogComponent(props) {
                     hash
                     author
                     commitMessage
-                    commitRelativeTime
                     commitFilesCount
                 }  
             }
@@ -232,7 +230,6 @@ export default function RepositoryCommitLogComponent(props) {
                 author
                 commitTime
                 commitMessage
-                commitRelativeTime
                 commitFilesCount
               }
             }
@@ -342,6 +339,42 @@ export default function RepositoryCommitLogComponent(props) {
     );
   }
 
+  function relativeCommitTimeCalculator(commitTime) {
+    let commitRelativeTime;
+
+    const now = new Date();
+    const timeDiff = moment.duration(moment(now).diff(commitTime));
+
+    const days = Math.floor(timeDiff.asDays());
+    const hours = Math.floor(timeDiff.asHours());
+    const minutes = Math.floor(timeDiff.asMinutes());
+
+    if (days > 0) {
+      if (days >= 30) {
+        const month = Math.floor(timeDiff.asMonths());
+        commitRelativeTime =
+          month === 1 ? month + " Month Ago" : month + " Months Ago";
+      } else if (days >= 365) {
+        const year = Math.floor(timeDiff.asYears());
+        commitRelativeTime =
+          year === 1 ? year + " Year Ago" : year + " Years Ago";
+      } else {
+        commitRelativeTime =
+          days === 1 ? days + " Day Ago" : days + " Days Ago";
+      }
+    } else if (hours > 0) {
+      commitRelativeTime =
+        hours === 1 ? hours + " Hour Ago" : hours + " Hours Ago";
+    } else if (minutes > 0) {
+      commitRelativeTime =
+        minutes === 1 ? minutes + " Minute Ago" : minutes + " Minutes Ago";
+    } else {
+      commitRelativeTime = "recent commit";
+    }
+
+    return commitRelativeTime;
+  }
+
   return (
     <>
       {searchbarComponent()}
@@ -355,12 +388,14 @@ export default function RepositoryCommitLogComponent(props) {
             author,
             commitTime,
             commitMessage,
-            commitRelativeTime,
             commitFilesCount,
           } = commit;
+
+          let commitRelativeTime = relativeCommitTimeCalculator(commitTime);
           const formattedCommitTime = moment(new Date(commitTime)).format(
             "MMM DD, YYYY"
           );
+
           return (
             <div
               id={`commitLogCard-${hash}`}
